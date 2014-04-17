8c609d57f1d1a6449d98b4e8cddd852622fc68b9
Steve <stsec07@moravian.edu>
2014-04-17 18:11:18 -0400

    King/Pawn highlight fix
    .

---
+++ src/Chess/ChessClock.java
@@ -0,0 +1,33 @@
+package Chess;
+
+public class ChessClock {
+	/**
+	 * 
+	 * @param seconds the number of seconds on the clock
+	 */
+	public ChessClock(long milliseconds){
+		
+	}
+	/**
+	 * Get the remaining time left on a chessClock
+	 * @return time left in milliseconds
+	 */
+	public long getTimeLeft(){
+		return 0;
+	}
+	/**
+	 * Start the chessClock
+	 */
+	public void start(){
+		
+	}
+	/**
+	 * Stop the chessClock
+	 */
+	public void stop(){
+		
+	}
+	public void addTime(){
+		
+	}
+}
